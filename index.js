79a1f56d6f47f14e7f26d0f8bae19aea485e107e
kalkidan Daniel <99416452+kal2944@users.noreply.github.com>
2022-07-27 18:52:27 -0700

    add mobile menu

--- index.js
+++ index.js
@@ -1,38 +1,22 @@
+const hamburgerBtn = document.querySelector('.hamburger-btn');
+const menu = document.querySelector('.menu');
+const link1 = document.querySelector('.link1');
+const link2 = document.querySelector('.link2');
+const link3 = document.querySelector('.link3');
 
-
-const navToggle = document.querySelector(".navbar_toggle");
-const links = document.querySelector(".main_nav");
-const bar1 = document.querySelector(".bar1");
-const bar2 = document.querySelector(".bar2");
-const bar3 = document.querySelector(".bar3");
-const navLink1 = document.querySelector(".nav_link1");
-const navLink2 = document.querySelector(".nav_link2");
-const navLink3 = document.querySelector(".nav_link3");
-
-navToggle.addEventListener('click', function() {
-    links.classList.toggle("show_nav");
-    bar1.classList.toggle("change1");
-    bar2.classList.toggle("change2");
-    bar3.classList.toggle("change3");
-})
-
-navLink1.addEventListener('click', function() {
-    links.classList.remove('show_nav');
-    bar1.classList.remove("change1");
-    bar2.classList.remove("change2");
-    bar3.classList.remove("change3");
-})
-
-navLink2.addEventListener('click', function() {
-    links.classList.remove('show_nav');
-    bar1.classList.remove("change1");
-    bar2.classList.remove("change2");
-    bar3.classList.remove("change3");
-})
-
-navLink3.addEventListener('click', function() {
-    links.classList.remove('show_nav');
-    bar1.classList.remove("change1");
-    bar2.classList.remove("change2");
-    bar3.classList.remove("change3");
-})
+hamburgerBtn.addEventListener('click', () => {
+  hamburgerBtn.classList.toggle('active');
+  menu.classList.toggle('active');
+});
+link1.addEventListener('click', () => {
+  hamburgerBtn.classList.toggle('active');
+  menu.classList.toggle('active');
+});
+link2.addEventListener('click', () => {
+  hamburgerBtn.classList.toggle('active');
+  menu.classList.toggle('active');
+});
+link3.addEventListener('click', () => {
+  hamburgerBtn.classList.toggle('active');
+  menu.classList.toggle('active');
+});
